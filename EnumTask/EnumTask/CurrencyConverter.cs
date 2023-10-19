7c4452a92f0930a8b207837d7f3b7bf59f54dbe0
cavidanalizade <145671273+cavidanalizade@users.noreply.github.com>
2023-10-19 10:18:26 +0400

    Add files via upload

---
+++ EnumTask/EnumTask/CurrencyConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumTask
+{
+    internal class CurrencyConverter
+    {
+        public  decimal ConvertCurrency(Currency targetCurrency, decimal amountInAZN)
+        {
+            decimal convertedAmount = 0;
+
+            switch (targetCurrency)
+            {
+                case Currency.Usd:
+
+                    convertedAmount = amountInAZN * 0.59M;
+                    break;
+                case Currency.Eur:
+                    convertedAmount = amountInAZN * 0.56M;
+                    break;
+                case Currency.TRY:
+                    convertedAmount = amountInAZN * 16.4826M;
+                    break;
+                case Currency.Rub:
+                    convertedAmount = amountInAZN * 56.9726M;
+                    break;
+            }
+
+            return convertedAmount;
+        }
+    }
+}
---
+++ EnumTask/EnumTask/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EnumTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Currency targetCurrency = Currency.Usd;
+            decimal amountInAZN = 100;
+            CurrencyConverter currencyConverter = new CurrencyConverter();
+
+            Currency targetCurrency2 = Currency.Eur;
+            Currency targetCurrency3 = Currency.TRY;
+            Currency targetCurrency4 = Currency.Rub;
+
+
+            decimal convertedAmount = currencyConverter.ConvertCurrency(targetCurrency, amountInAZN);
+            decimal convertedAmount2 = currencyConverter.ConvertCurrency(targetCurrency2, amountInAZN);
+            decimal convertedAmount3 = currencyConverter.ConvertCurrency(targetCurrency3, amountInAZN);
+            decimal convertedAmount4 = currencyConverter.ConvertCurrency(targetCurrency4, amountInAZN);
+
+
+
+
+
+
+            Console.WriteLine($"{amountInAZN} AZN is equivalent to {convertedAmount} {targetCurrency}");
+            Console.WriteLine($"{amountInAZN} AZN is equivalent to {convertedAmount2} {targetCurrency2}");
+            Console.WriteLine($"{amountInAZN} AZN is equivalent to {convertedAmount3} {targetCurrency3}");
+            Console.WriteLine($"{amountInAZN} AZN is equivalent to {convertedAmount4} {targetCurrency4}");
+
+
+
+
+        }
+    }
+}
